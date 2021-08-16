85b159233e7754148787802b1744682351bb449d
semibroiled <amitavcmostafa@gmx.de>
2021-08-16 19:29:05 +0200

    first commit

---
+++ trial.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+
+function square(num) {
+    if (num === 0 || num === 1) {
+      return num;
+    }
+    return num*num;
+  }
+  
+  let n = square(8);
+  console.log(n);
+let a = 'hello world'
+console.log(a)
